98538fc0f46ca71451b8e6f8bd8e1db9ebf5b5eb
opilane <opilane@HP-CZC0359NN6>
2025-01-29 11:38:54 +0200

    Tegin PageViewModel ja CalcPage

---
+++ SciCalc/SciCalc/ViewModels/CalculatorPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewModels
+{
+    [INotifyPropertyChanged]
+    internal partial class CalculatorPageViewModel
+    {
+    }
+}
